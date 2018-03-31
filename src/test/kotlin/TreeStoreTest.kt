3a4785c8f3a24a58a748a92fcd0c6c4f1bfc4d20
Hai Pham <viethai.pham@holmusk.com>
2018-04-01 00:41:44 +0800

    test(treeStore): added tests for RxTreeStore and ReduxProcessor

---
+++ src/test/kotlin/TreeStoreTest.kt
@@ -0,0 +1,89 @@
+import common.ReduxActionType
+import common.ReduxReducer
+import common.ReduxStoreType
+import io.reactivex.processors.BehaviorProcessor
+import org.testng.Assert
+import org.testng.annotations.Test
+import rxstore.RxTreeStore
+import java.util.*
+
+/**
+ * Created by haipham on 1/4/18.
+ */
+
+class TreeStoreTest {
+  enum class Action(val value: Int): ReduxActionType {
+    ADD_ONE(1),
+    ADD_TWO(2),
+    ADD_THREE(3),
+    ADD_FOUR(4),
+    ADD_FIVE(5);
+
+    companion object {
+      fun randomAction(): Action {
+        val values = Action.values()
+        val rand = Random()
+        return values[rand.nextInt(values.size)]
+      }
+    }
+
+    fun updateFn(): (Int) -> Int {
+      return { it + value }
+    }
+  }
+
+  companion object {
+    const val path = "a.b.c"
+    const val times = 10000
+    const val actionsPerIteration = 100
+  }
+
+  class Reducer: ReduxReducer<TreeStateType<Int>> {
+    override operator fun invoke(previous: TreeStateType<Int>,
+                                 action: ReduxActionType): TreeStateType<Int> {
+      return when (action) {
+        is Action -> previous.mapValue(path) {
+          it.catchFailure(0).map { it + action.value }
+        }
+
+        else -> previous
+      }
+    }
+  }
+
+  fun test_treeStore_shouldWork(store: ReduxStoreType<TreeStateType<Int>>): Int {
+    /// Setup
+    var original = 0
+
+    /// When
+    for (i in 0 until times) {
+      val actions = (0 until actionsPerIteration).map { Action.randomAction() }
+      actions.forEach { original = it.updateFn()(original) }
+      store.dispatch(actions)
+    }
+
+    /// Then
+    val lastState = store.lastState
+    Assert.assertEquals(lastState.valueAt(path).value, original)
+    return original
+  }
+
+  @Test
+  fun test_rxTreeStore_shouldWork() {
+    /// Setup
+    val store = RxTreeStore.create(Reducer())
+    val stateProcessor = BehaviorProcessor.createDefault(TreeState.empty<Int>())
+    val valueProcessor = BehaviorProcessor.createDefault(0)
+    store.stateStream.subscribe(stateProcessor)
+    store.valueStream(path).map { it.getOrElse(0) }.subscribe(valueProcessor)
+
+    /// When
+    val finalValue = test_treeStore_shouldWork(store)
+
+    /// Then
+    val lastState = stateProcessor.value
+    val lastValue = valueProcessor.value
+    Assert.assertEquals(lastState.valueAt(path).value, finalValue)
+    Assert.assertEquals(lastValue, finalValue)
+  }
+}
