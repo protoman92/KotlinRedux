d90761e25f012c1495f48fd9b779706635d74bc8
Hai Pham <swiften.svc@gmail.com>
2018-12-16 16:28:18 +0800

    test(middleware): add tests for combineMiddlewares and applyMiddlewares

--- middleware/src/main/kotlin/org/swiften/redux/ReduxMiddleware.kt
+++ middleware/src/main/kotlin/org/swiften/redux/ReduxMiddleware.kt
@@ -23,18 +23,6 @@ class ReduxMiddleware {
   }
 
   /**
-   * [Input] for middlewares that includes some functionalities from
-   * [Redux.IStore].
-   */
-  class Input<State>(val lastState: () -> State)
-
-  /**
-   * Use this [DispatchWrapper] to track the ordering of [dispatch] wrapping
-   * using [id].
-   */
-  class DispatchWrapper(val id: String, val dispatch: (Redux.IAction) -> Unit)
-
-  /**
    * Implement [IProvider] to provide a middleware instance.
    */
   interface IProvider<State> {
@@ -46,28 +34,64 @@ class ReduxMiddleware {
   }
 
   /**
-   * Combine [middlewares] into a master [IMiddleware] and apply it to a
-   * [Redux.IStore.dispatch] to produce a [DispatchWrapper].
+   * [Input] for middlewares that includes some functionalities from
+   * [Redux.IStore].
    */
-  fun <State> combineMiddlewares(
-    middlewares: Collection<IMiddleware<State>>
-  ): (Redux.IStore<State>) -> DispatchWrapper {
-    return fun(store): DispatchWrapper {
-      val input = Input { store.lastState() }
-      val rootWrapper = DispatchWrapper("root") { store.dispatch(it) }
-      if (middlewares.isEmpty()) return rootWrapper
+  class Input<State>(val lastState: () -> State)
 
-      return middlewares.reduce { acc, middleware ->
-        object : IMiddleware<State> {
-          override operator fun invoke(input: Input<State>): IDispatchMapper {
-            return object : IDispatchMapper {
-              override operator fun invoke(wrapper: DispatchWrapper): DispatchWrapper {
-                return acc(input)(middleware(input)(wrapper))
+  /**
+   * Use this [DispatchWrapper] to track the ordering of [dispatch] wrapping
+   * using [id].
+   */
+  class DispatchWrapper(val id: String, val dispatch: Redux.IDispatcher)
+
+  /**
+   * Enhance a [store] by overriding its [Redux.IStore.dispatch] with
+   * [dispatch].
+   */
+  private class EnhancedStore<State>(
+    val store: Redux.IStore<State>,
+    override val dispatch: Redux.IDispatcher
+  ): Redux.IStore<State> by store
+
+  companion object {
+    /**
+     * Combine [middlewares] into a master [IMiddleware] and apply it to a
+     * [Redux.IStore.dispatch] to produce a [DispatchWrapper].
+     */
+    fun <State> combineMiddlewares(
+      middlewares: Collection<IMiddleware<State>>
+    ): (Redux.IStore<State>) -> DispatchWrapper {
+      return fun(store): DispatchWrapper {
+        val input = Input { store.lastState() }
+        val rootWrapper = DispatchWrapper("root", store.dispatch)
+        if (middlewares.isEmpty()) return rootWrapper
+
+        return middlewares.reduce { acc, middleware ->
+          object : IMiddleware<State> {
+            override operator fun invoke(input: Input<State>): IDispatchMapper {
+              return object : IDispatchMapper {
+                override operator fun invoke(wrapper: DispatchWrapper): DispatchWrapper {
+                  return acc(input)(middleware(input)(wrapper))
+                }
               }
             }
           }
-        }
-      }(input)(rootWrapper)
+        }(input)(rootWrapper)
+      }
+    }
+
+    /**
+     * Apply [middlewares] to a [Redux.IStore] instance to get an enhanced
+     * [Redux.IStore].
+     */
+    fun <State> applyMiddlewares(
+      middlewares: Collection<IMiddleware<State>>
+    ): (Redux.IStore<State>) -> Redux.IStore<State> {
+      return fun(store): Redux.IStore<State> {
+        val wrapper = this.combineMiddlewares(middlewares)(store)
+        return EnhancedStore(store, wrapper.dispatch)
+      }
     }
   }
 }
---
+++ middleware/src/test/kotlin/org/swiften/redux/ReduxMiddlewareTest.kt
@@ -0,0 +1,107 @@
+package org.swiften.redux
+
+import org.testng.Assert
+import org.testng.annotations.Test
+
+/**
+ * Created by haipham on 2018-12-16.
+ */
+class ReduxMiddlewareTest {
+  @Test
+  fun `Applying middlewares to a store should produce correct order`() {
+    /// Setup
+    val store = SimpleReduxStore(0, object : Redux.IReducer<Int> {
+      override operator fun invoke(previous: Int, action: Redux.IAction): Int {
+        return previous
+      }
+    })
+
+    val ordering = arrayListOf<Int>()
+
+    val wrappedStore = ReduxMiddleware.applyMiddlewares<Int>(arrayListOf(
+      object : ReduxMiddleware.IMiddleware<Int> {
+        override operator fun invoke(
+          input: ReduxMiddleware.Input<Int>
+        ): ReduxMiddleware.IDispatchMapper {
+          return object : ReduxMiddleware.IDispatchMapper {
+            override operator fun invoke(
+              wrapper: ReduxMiddleware.DispatchWrapper
+            ): ReduxMiddleware.DispatchWrapper {
+              return ReduxMiddleware.DispatchWrapper(
+                "$wrapper.id-$1",
+                object : Redux.IDispatcher {
+                  override operator fun invoke(action: Redux.IAction) {
+                    wrapper.dispatch(action)
+                    ordering.add(1)
+                  }
+                })
+            }
+          }
+        }
+      },
+      object : ReduxMiddleware.IMiddleware<Int> {
+        override operator fun invoke(
+          input: ReduxMiddleware.Input<Int>
+        ): ReduxMiddleware.IDispatchMapper {
+          return object : ReduxMiddleware.IDispatchMapper {
+            override operator fun invoke(
+              wrapper: ReduxMiddleware.DispatchWrapper
+            ): ReduxMiddleware.DispatchWrapper {
+              return ReduxMiddleware.DispatchWrapper(
+                "$wrapper.id-$2",
+                object : Redux.IDispatcher {
+                  override operator fun invoke(action: Redux.IAction) {
+                    wrapper.dispatch(action)
+                    ordering.add(2)
+                  }
+                })
+            }
+          }
+        }
+      },
+      object : ReduxMiddleware.IMiddleware<Int> {
+        override operator fun invoke(
+          input: ReduxMiddleware.Input<Int>
+        ): ReduxMiddleware.IDispatchMapper {
+          return object : ReduxMiddleware.IDispatchMapper {
+            override operator fun invoke(
+              wrapper: ReduxMiddleware.DispatchWrapper
+            ): ReduxMiddleware.DispatchWrapper {
+              return ReduxMiddleware.DispatchWrapper(
+                "$wrapper.id-$3",
+                object : Redux.IDispatcher {
+                  override operator fun invoke(action: Redux.IAction) {
+                    wrapper.dispatch(action)
+                    ordering.add(3)
+                  }
+                })
+            }
+          }
+        }
+      }
+    ))(store)
+
+    /// When
+    wrappedStore.dispatch(ReduxPreset.DefaultAction.Dummy)
+    wrappedStore.dispatch(ReduxPreset.DefaultAction.Dummy)
+    wrappedStore.dispatch(ReduxPreset.DefaultAction.Dummy)
+
+    /// Then
+    Assert.assertEquals(ordering, arrayListOf(3, 2, 1, 3, 2, 1, 3, 2, 1))
+  }
+
+  fun `Applying middlewares with empty middleware list`() {
+    /// Setup
+    val store = SimpleReduxStore(0, object : Redux.IReducer<Int> {
+      override operator fun invoke(previous: Int, action: Redux.IAction): Int {
+        return previous
+      }
+    })
+
+    /// When
+    val wrapper = ReduxMiddleware.combineMiddlewares<Int>(arrayListOf())(store)
+
+    /// Then
+    Assert.assertEquals(wrapper.id, "root")
+  }
+}
