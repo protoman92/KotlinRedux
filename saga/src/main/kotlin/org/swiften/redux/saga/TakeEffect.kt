86460b6e448dc09a58c7e232dff25d65e5633bc2
Hai Pham <swiften.svc@gmail.com>
2018-12-24 18:03:52 +0700

    feat(take-effect): add Take, TakeEvery and TakeLatest effects

---
+++ saga/src/main/kotlin/org/swiften/redux/saga/TakeEffect.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) haipham 2018. All rights reserved.
+ * Any attempt to reproduce this source code in any form shall be met with legal actions.
+ */
+
+package org.swiften.redux.saga
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.launch
+import org.swiften.redux.core.Redux
+
+/**
+ * Created by haipham on 2018/12/23.
+ */
+/**
+ * [TakeEffect] instances produces streams that filter [Redux.IAction] with
+ * [extract] and pluck out the appropriate ones to perform additional work on
+ * with [block].
+ */
+internal abstract class TakeEffect<State, P, R>(
+  private val extract: (Redux.IAction) -> P?,
+  private val block: suspend CoroutineScope.(P) -> ReduxSaga.IEffect<State, R>
+) : ReduxSaga.IEffect<State, R> {
+  /**
+   * Flatten an [ReduxSaga.Output] that streams [ReduxSaga.Output] to access
+   * the values streamed by the inner [ReduxSaga.Output].
+   */
+  abstract fun flattenOutput(
+    output: ReduxSaga.Output<ReduxSaga.Output<R>>
+  ): ReduxSaga.Output<R>
+
+  @ExperimentalCoroutinesApi
+  override operator fun invoke(input: ReduxSaga.Input<State>): ReduxSaga.Output<R> {
+    val actionChannel = Channel<Redux.IAction>()
+
+    return flattenOutput(ReduxSaga.Output(input.scope,
+      input.scope.produce {
+        for (action in actionChannel) {
+          val param = this@TakeEffect.extract(action)
+
+          if (param != null) {
+            this.send(this@TakeEffect.block(this, param).invoke(input))
+          }
+        }
+      }, object : Redux.IDispatcher {
+        override operator fun invoke(action: Redux.IAction) {
+          input.scope.launch { actionChannel.send(action) }
+        }
+      }))
+  }
+}
