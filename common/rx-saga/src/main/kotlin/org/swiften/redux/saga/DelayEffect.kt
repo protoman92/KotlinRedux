9ecaec8ec255dff95eaa0c5ebbd95a9b18852552
Hai Pham <swiften.svc@gmail.com>
2019-01-07 03:52:03 +0700

    feat(delay-effect): add delay effect

---
+++ common/rx-saga/src/main/kotlin/org/swiften/redux/saga/DelayEffect.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) haipham 2019. All rights reserved.
+ * Any attempt to reproduce this source code in any form shall be met with legal actions.
+ */
+
+package org.swiften.redux.saga
+
+/** Created by haipham on 2019/01/07 */
+/**
+ * [ReduxSagaEffect] whose [ReduxSaga.Output] delays emissions by
+ * [delayMillis]
+ */
+internal class DelayEffect<State, R>(
+  private val source: ReduxSagaEffect<State, R>,
+  private val delayMillis: Long
+) : ReduxSagaEffect<State, R> {
+  override fun invoke(p1: ReduxSaga.Input<State>) =
+    this.source.invoke(p1).delay(this.delayMillis)
+}
+
+/** Invoke a [DelayEffect] on [this] */
+fun <State, R> ReduxSagaEffect<State, R>.delay(delayMillis: Long) =
+  ReduxSagaHelper.delay(this, delayMillis)
