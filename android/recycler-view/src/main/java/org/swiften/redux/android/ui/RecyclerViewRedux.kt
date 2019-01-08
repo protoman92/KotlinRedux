f57b657603fe142304a13778067720b1337b57c6
Hai Pham <swiften.svc@gmail.com>
2019-01-08 19:41:27 +0700

    feat(recycler-view): add preliminary prop injector method for recycler view

---
+++ android/recycler-view/src/main/java/org/swiften/redux/android/ui/RecyclerViewRedux.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) haipham 2019. All rights reserved.
+ * Any attempt to reproduce this source code in any form shall be met with legal actions.
+ */
+
+package org.swiften.redux.android.ui
+
+import androidx.recyclerview.widget.RecyclerView
+import org.swiften.redux.ui.ReduxUI
+
+/** Created by haipham on 2019/01/08 */
+/** Injector props for a [RecyclerView.Adapter] */
+fun <State, VH> ReduxUI.IPropInjector<State>.injectProps(
+  adapter: RecyclerView.Adapter<VH>
+) : RecyclerView.Adapter<VH> where VH : RecyclerView.ViewHolder = adapter
