1146197d62db8c67f1cbc3979915e55ac2f819cf
Hai Pham <swiften.svc@gmail.com>
2019-01-12 17:00:26 +0700

    feat(android-sample): add music detail fragment

---
+++ sample/android/src/main/java/org/swiften/redux/android/sample/MusicDetailFragment.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) haipham 2019. All rights reserved.
+ * Any attempt to reproduce this source code in any form shall be met with legal actions.
+ */
+
+package org.swiften.redux.android.sample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_music_detail.trackName
+import org.swiften.redux.ui.IReduxPropContainer
+import org.swiften.redux.ui.IReduxStatePropMapper
+import org.swiften.redux.ui.StaticProps
+import org.swiften.redux.ui.VariableProps
+import kotlin.properties.Delegates
+
+/** Created by haipham on 2019/01/12 */
+class MusicDetailFragment : Fragment(),
+  IReduxPropContainer<State, MusicDetailFragment.S, Unit>,
+  IReduxStatePropMapper<State, Int, MusicDetailFragment.S> by MusicDetailFragment {
+  class S(val track: MusicTrack?)
+
+  companion object : IReduxStatePropMapper<State, Int, S> {
+    override fun mapState(state: State, outProps: Int) =
+      S(state.musicResult?.results?.elementAtOrNull(outProps))
+  }
+
+  override lateinit var staticProps: StaticProps<State>
+
+  override var variableProps
+    by Delegates.observable<VariableProps<S, Unit>?>(null) { _, _, p ->
+      p?.next?.track?.also { this.trackName.text = it.trackName }
+    }
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? = inflater.inflate(R.layout.fragment_music_detail, container, false)
+
+  override fun beforePropInjectionStarts() {}
+
+  override fun afterPropInjectionEnds() {}
+}
