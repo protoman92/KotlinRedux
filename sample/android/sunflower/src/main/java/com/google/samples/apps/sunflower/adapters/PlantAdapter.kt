b9db0ef997ac7bbf39fa3bff5a2328c5db61ab5a
Hai Pham <swiften.svc@gmail.com>
2019-01-17 10:10:21 +0700

    style(sunflower-sample): fix indentation for PlantAdapter

--- sample/android/sunflower/src/main/java/com/google/samples/apps/sunflower/adapters/PlantAdapter.kt
+++ sample/android/sunflower/src/main/java/com/google/samples/apps/sunflower/adapters/PlantAdapter.kt
@@ -32,48 +32,46 @@ import com.google.samples.apps.sunflower.databinding.ListItemPlantBinding
  * Adapter for the [RecyclerView] in [PlantListFragment].
  */
 class PlantAdapter : ListAdapter<Plant, PlantAdapter.ViewHolder>(PlantDiffCallback()) {
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val plant = getItem(position)
-        holder.apply {
-            bind(createOnClickListener(plant.plantId), plant)
-            itemView.tag = plant
-        }
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    val plant = getItem(position)
+    holder.apply {
+      bind(createOnClickListener(plant.plantId), plant)
+      itemView.tag = plant
     }
+  }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(ListItemPlantBinding.inflate(
-                LayoutInflater.from(parent.context), parent, false))
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    return ViewHolder(ListItemPlantBinding.inflate(
+      LayoutInflater.from(parent.context), parent, false))
+  }
+
+  private fun createOnClickListener(plantId: String): View.OnClickListener {
+    return View.OnClickListener {
+      val direction = PlantListFragmentDirections.ActionPlantListFragmentToPlantDetailFragment(plantId)
+      it.findNavController().navigate(direction)
     }
+  }
 
-    private fun createOnClickListener(plantId: String): View.OnClickListener {
-        return View.OnClickListener {
-            val direction = PlantListFragmentDirections.ActionPlantListFragmentToPlantDetailFragment(plantId)
-            it.findNavController().navigate(direction)
-        }
+  class ViewHolder(
+    private val binding: ListItemPlantBinding
+  ) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(listener: View.OnClickListener, item: Plant) {
+      binding.apply {
+        clickListener = listener
+        plant = item
+        executePendingBindings()
+      }
     }
-
-    class ViewHolder(
-        private val binding: ListItemPlantBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(listener: View.OnClickListener, item: Plant) {
-            binding.apply {
-                clickListener = listener
-                plant = item
-                executePendingBindings()
-            }
-        }
-    }
+  }
 }
 
 private class PlantDiffCallback : DiffUtil.ItemCallback<Plant>() {
+  override fun areItemsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+    return oldItem.plantId == newItem.plantId
+  }
 
-    override fun areItemsTheSame(oldItem: Plant, newItem: Plant): Boolean {
-        return oldItem.plantId == newItem.plantId
-    }
-
-    override fun areContentsTheSame(oldItem: Plant, newItem: Plant): Boolean {
-        return oldItem == newItem
-    }
+  override fun areContentsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+    return oldItem == newItem
+  }
 }
