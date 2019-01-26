ba435a6181fb10f2919798227a2dd1d1913789e3
Hai Pham <swiften.svc@gmail.com>
2019-01-26 22:57:20 +0800

    feat(async-middleware): add async middleware to dispatch actions on background threads, phasing out async store

---
+++ common/common-async/src/main/kotlin/org/swiften/redux/async/AsyncMiddleware.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) haipham 2019. All rights reserved.
+ * Any attempt to reproduce this source code in any form shall be met with legal actions.
+ */
+
+package org.swiften.redux.async
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import org.swiften.redux.core.DefaultReduxAction
+import org.swiften.redux.middleware.DispatchMapper
+import org.swiften.redux.middleware.DispatchWrapper
+import org.swiften.redux.middleware.IMiddleware
+import org.swiften.redux.middleware.MiddlewareInput
+import kotlin.coroutines.CoroutineContext
+
+/** Created by haipham on 2019/01/26 */
+/** [IMiddleware] implementation that calls [DispatchWrapper.dispatch] on another thread */
+internal class AsyncMiddleware<GlobalState>(private val context: CoroutineContext) :
+  IMiddleware<GlobalState> {
+  override fun invoke(p1: MiddlewareInput<GlobalState>): DispatchMapper {
+    return { wrapper ->
+      DispatchWrapper("${wrapper.id}-async") { action ->
+        when (action) {
+          is DefaultReduxAction.Deinitialize -> this@AsyncMiddleware.context.cancel()
+          else -> GlobalScope.launch(this@AsyncMiddleware.context) { wrapper.dispatch(action) }
+        }
+      }
+    }
+  }
+}
+
+/** Create a [AsyncMiddleware] with [context] */
+fun <GlobalState> createAsyncMiddleware(context: CoroutineContext = SupervisorJob()):
+  IMiddleware<GlobalState> = AsyncMiddleware(context)
