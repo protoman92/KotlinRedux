94d35cf99307ffa1d797f5f16e3c8f25bff9eab1
Hai Pham <swiften.svc@gmail.com>
2019-02-06 18:10:12 +0800

    docs(common-saga): add detailed documentation for common-saga

--- common/common-saga/src/main/kotlin/org/swiften/redux/saga/common/CommonExtension.kt
+++ common/common-saga/src/main/kotlin/org/swiften/redux/saga/common/CommonExtension.kt
@@ -10,72 +10,170 @@ import kotlinx.coroutines.Deferred
 import org.swiften.redux.core.IReduxAction
 
 /** Created by haipham on 2019/01/26/1 */
-/** Cast the emission from the current [ISagaEffect] to [R2] if possible */
+/**
+ * Cast the emission from the current [ISagaEffect] to [R2] if possible.
+ * @receiver A [SagaEffect] instance.
+ * @param R2 The result emission type.
+ * @param cls The [Class] of [R2].
+ * @return A [SagaEffect] instance.
+ */
 fun <R2> SagaEffect<*>.cast(cls: Class<R2>) = this.filter { cls.isInstance(it) }.map { cls.cast(it) }
 
-/** Cast the emission from the current [ISagaEffect] to [R2] if possible */
+/**
+ * Cast the emission from the current [ISagaEffect] to [R2] if possible.
+ * @receiver A [SagaEffect] instance.
+ * @param R2 The result emission type.
+ * @return A [SagaEffect] instance.
+ */
 inline fun <reified R2> SagaEffect<*>.cast() = this.filter { it is R2 }.map { it as R2 }
 
-/** Catch [Throwable] from upstream with [catcher] */
+/**
+ * Catch [Throwable] from upstream with [catcher].
+ * @receiver A [SagaEffect] instance.
+ * @param R The result emission type.
+ * @param catcher Function that catches [Throwable].
+ * @return A [SagaEffect] instance.
+ */
 fun <R> SagaEffect<R>.catchError(catcher: (Throwable) -> R): SagaEffect<R> {
   return this.transform(CommonEffects.catchError(catcher))
 }
 
-/** Invoke a [SuspendCatchErrorEffect] on [this] */
+/**
+ * Invoke a [SuspendCatchErrorEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param R The result emission type.
+ * @param catcher Function that catches [Throwable].
+ * @return A [SagaEffect] instance.
+ */
 fun <R> SagaEffect<R>.catchSuspend(catcher: suspend CoroutineScope.(Throwable) -> R): SagaEffect<R> {
   return this.transform(CommonEffects.catchErrorSuspend(catcher))
 }
 
-/** Invoke a [AsyncCatchErrorEffect] on [this] */
+/**
+ * Invoke a [AsyncCatchErrorEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param R The result emission type.
+ * @param catcher Function that catches [Throwable].
+ * @return A [SagaEffect] instance.
+ */
 fun <R> SagaEffect<R>.catchAsync(catcher: suspend CoroutineScope.(Throwable) -> Deferred<R>): SagaEffect<R> {
   return this.transform(CommonEffects.catchErrorAsync(catcher))
 }
 
-/** Invoke a [DelayEffect] on [this] */
+/**
+ * Invoke a [DelayEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param R The result emission type.
+ * @param millis The delay time in milliseconds.
+ * @return A [SagaEffect] instance.
+ */
 fun <R> SagaEffect<R>.delay(millis: Long) = this.transform(CommonEffects.delay(millis))
 
-/** Invoke a [DoOnValueEffect] on [this] */
+/**
+ * Invoke a [DoOnValueEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param R The result emission type.
+ * @param performer Function that performs side effects on [R].
+ * @return A [SagaEffect] instance.*
+ */
 fun <R> SagaEffect<R>.doOnValue(performer: (R) -> Unit): SagaEffect<R> {
   return this.transform(CommonEffects.doOnValue(performer))
 }
 
-/** Invoke a [FilterEffect] on the current [ISagaEffect] */
+/**
+ * Invoke a [FilterEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param R The result emission type.
+ * @param predicate Function that performs logic checks on [R].
+ * @return A [SagaEffect] instance.
+ */
 fun <R> SagaEffect<R>.filter(predicate: (R) -> Boolean): SagaEffect<R> {
   return this.transform(CommonEffects.filter(predicate))
 }
 
-/** Invoke a [MapEffect] on the current [ISagaEffect] */
+/**
+ * Invoke a [MapEffect] on the current [ISagaEffect].
+ * @receiver A [SagaEffect] instance.
+ * @param R The source emission type.
+ * @param R2 The result emission type.
+ * @param transformer Function that transforms [R] to [R2].
+ * @return A [SagaEffect] instance.
+ */
 fun <R, R2> SagaEffect<R>.map(transformer: (R) -> R2): SagaEffect<R2> {
   return this.transform(CommonEffects.map(transformer))
 }
 
-/** Invoke a [AsyncMapEffect] on [this] */
+/**
+ * Invoke a [AsyncMapEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param P The source emission type.
+ * @param R The result emission type.
+ * @param transformer Function that transforms [R] to [R2].
+ * @return A [SagaEffect] instance.
+ */
 fun <P, R> SagaEffect<P>.mapAsync(transformer: suspend CoroutineScope.(P) -> Deferred<R>): SagaEffect<R> {
   return this.transform(CommonEffects.mapAsync(transformer))
 }
 
 /**
  * Invoke a [CompactMapEffect] on [this].
+ * @receiver A [SagaEffect] instance.
  * @param R The result emission type.
  * @return An [ISagaEffect] instance.
  */
 fun <R : Any> SagaEffect<R?>.compactMap() = this.transform<R>(CommonEffects.compactMap())
 
-/** Invoke a [PutEffect] on the current [CommonEffects] */
+/**
+ * Invoke a [PutEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param P The source emission type.
+ * @param actionCreator Function that creates [IReduxAction] from [P].
+ * @return A [SagaEffect] instance.
+ */
 fun <P> SagaEffect<P>.put(actionCreator: (P) -> IReduxAction): SagaEffect<Any> {
   return this.transform(CommonEffects.put(actionCreator))
 }
 
-/** Invoke a [RetryEffect] on [this] */
+/**
+ * Invoke a [RetryEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param R The result emission type.
+ * @param times The number of times to retry.
+ * @return A [SagaEffect] instance.
+ */
 fun <R> SagaEffect<R>.retry(times: Long) = this.transform(CommonEffects.retry(times))
+
+/**
+ * Invoke a [RetryEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param R The result emission type.
+ * @param times The number of times to retry.
+ * @return A [SagaEffect] instance.
+ */
 fun <R> SagaEffect<R>.retry(times: Int) = this.retry(times.toLong())
 
-/** Invoke a [SuspendMapEffect] on [this] */
+/**
+ * Invoke a [SuspendMapEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param P The source emission type.
+ * @param R The result emission type.
+ * @param transformer Function that transforms [R] to [R2].
+ * @return A [SagaEffect] instance.
+ */
 fun <P, R> SagaEffect<P>.mapSuspend(transformer: suspend CoroutineScope.(P) -> R): SagaEffect<R> {
   return this.transform(CommonEffects.mapSuspend(transformer))
 }
 
-/** Invoke a [ThenEffect] on the current [ISagaEffect] */
+/**
+ * Invoke a [ThenEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param R The first source emission type.
+ * @param R2 The second source emission type.
+ * @param R3 The result emission type.
+ * @param effect The [ISagaEffect] to perform after [this].
+ * @param combiner Function that combines [R] and [R2] to produce [R3].
+ * @return A [SagaEffect] instance.
+ */
 fun <R, R2, R3> SagaEffect<R>.then(
   effect: ISagaEffect<R2>,
   combiner: Function2<R, R2, R3>
@@ -83,11 +181,31 @@ fun <R, R2, R3> SagaEffect<R>.then(
   return CommonEffects.then(this, effect, combiner)
 }
 
-/** Invoke a [ThenEffect] but ignore emissions from [this] */
+/**
+ * Invoke a [ThenEffect] but ignore emissions from [this]
+ * @receiver A [SagaEffect] instance.
+ * @param R The first source emission type.
+ * @param R2 The result emission type.
+ * @param effect The [ISagaEffect] to perform after [this].
+ * @return A [SagaEffect] instance.
+ */
 fun <R, R2> SagaEffect<R>.then(effect: ISagaEffect<R2>) = this.then(effect) { _, b -> b }
 
-/** Invoke [then] with a single [value] */
+/**
+ * Invoke [then] with a single [value].
+ * @receiver A [SagaEffect] instance.
+ * @param R The first source emission type.
+ * @param R2 The result emission type.
+ * @param value The value to emit after [this].
+ * @return A [SagaEffect] instance.
+ */
 fun <R, R2> SagaEffect<R>.justThen(value: R2) = this.map { value }
 
-/** Invoke a [TimeoutEffect] on [this] */
+/**
+ * Invoke a [TimeoutEffect] on [this].
+ * @receiver A [SagaEffect] instance.
+ * @param R The result emission type.
+ * @param millis The timeout time in milliseconds.
+ * @return A [SagaEffect] instance.
+ */
 fun <R> SagaEffect<R>.timeout(millis: Long) = this.transform(CommonEffects.timeout(millis))
--- common/common-saga/src/main/kotlin/org/swiften/redux/saga/common/SagaMiddleware.kt
+++ common/common-saga/src/main/kotlin/org/swiften/redux/saga/common/SagaMiddleware.kt
@@ -8,27 +8,31 @@ package org.swiften.redux.saga.common
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.cancel
 import org.swiften.redux.core.DefaultReduxAction
 import org.swiften.redux.core.DispatchMapper
 import org.swiften.redux.core.DispatchWrapper
 import org.swiften.redux.core.IMiddleware
 import org.swiften.redux.core.IReduxAction
 import org.swiften.redux.core.MiddlewareInput
+import kotlin.coroutines.CoroutineContext
 
 /** Created by haipham on 2018/12/22 */
 /**
  * [IMiddleware] implementation for [ISagaEffect]. Every time an [IReduxAction] is received, call
  * [ISagaOutput.onAction].
+ * @param effects The [List] of [ISagaEffect] to run.
+ * @param context The [CoroutineContext] with which to perform asynchronous work on.
  */
-internal class SagaMiddleware(private val effects: Collection<ISagaEffect<*>>) :
+internal class SagaMiddleware(
+  private val effects: Collection<ISagaEffect<*>>,
+  private val context: CoroutineContext
+) :
   IMiddleware<Any> {
   override fun invoke(p1: MiddlewareInput<Any>): DispatchMapper {
     return { wrapper ->
-      val job = SupervisorJob()
-
       val scope = object : CoroutineScope {
-        override val coroutineContext = Dispatchers.Default + job
+        override val coroutineContext = Dispatchers.Default + this@SagaMiddleware.context
       }
 
       val sagaInput = SagaInput(scope, p1.lastState, p1.dispatch)
@@ -42,14 +46,32 @@ internal class SagaMiddleware(private val effects: Collection<ISagaEffect<*>>) :
         /** If [action] is [DefaultReduxAction.Deinitialize], dispose of all [ISagaOutput] */
         if (action == DefaultReduxAction.Deinitialize) {
           outputs.forEach { it.dispose() }
-          job.cancelChildren()
+          scope.coroutineContext.cancel()
         }
       }
     }
   }
 }
 
-/** Create a [SagaMiddleware] with [effects] */
+/**
+ * Create a [SagaMiddleware] with [effects].
+ * @param effects See [SagaMiddleware.effects].
+ * @param context See [SagaMiddleware.context].
+ * @return A [SagaMiddleware] instance.
+ */
+internal fun createSagaMiddleware(
+  effects: Collection<ISagaEffect<*>>,
+  context: CoroutineContext = SupervisorJob()
+): IMiddleware<Any> {
+  return SagaMiddleware(effects, context)
+}
+
+/**
+ * Create a [SagaMiddleware] with [effects] and a default [CoroutineContext] so this
+ * [SagaMiddleware] does not have to share its [CoroutineContext] with any other user.
+ * @param effects See [SagaMiddleware.effects].
+ * @return A [SagaMiddleware] instance.
+ */
 fun createSagaMiddleware(effects: Collection<ISagaEffect<*>>): IMiddleware<Any> {
-  return SagaMiddleware(effects)
+  return createSagaMiddleware(effects, SupervisorJob())
 }
