b4aaa3ec122dec0241293867d87914c66d46ab6f
Hai Pham <swiften.svc@gmail.com>
2019-02-07 22:46:26 +0800

    style(general): clean up code basic on IDE analysis

--- sample-android/sample-sunflower/src/main/java/com/google/samples/apps/sunflower/dependency/Dependency.kt
+++ sample-android/sample-sunflower/src/main/java/com/google/samples/apps/sunflower/dependency/Dependency.kt
@@ -6,7 +6,6 @@
 package com.google.samples.apps.sunflower.dependency
 
 import com.squareup.picasso.Picasso
-import org.swiften.redux.ui.IPropInjector
 
 /** Created by haipham on 2019/01/17 */
 interface IPicassoProvider {
