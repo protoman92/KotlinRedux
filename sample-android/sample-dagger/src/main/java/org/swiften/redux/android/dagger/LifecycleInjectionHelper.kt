5f27fde597a70d009cad31027b5808606bdf4b93
Hai Pham <swiften.svc@gmail.com>
2019-02-21 17:53:40 +0800

    feat(android-lifecycle): add ILifecycleInjectionHelper to facilitate Dagger

---
+++ sample-android/sample-dagger/src/main/java/org/swiften/redux/android/dagger/LifecycleInjectionHelper.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) haipham 2019. All rights reserved.
+ * Any attempt to reproduce this source code in any form shall be met with legal actions.
+ */
+
+package org.swiften.redux.android.dagger
+
+import androidx.lifecycle.LifecycleOwner
+import org.swiften.redux.android.ui.lifecycle.ILifecycleOwnerInjectionHelper
+import org.swiften.redux.android.ui.lifecycle.injectLifecycle
+import org.swiften.redux.ui.IPropInjector
+
+/** Created by viethai.pham on 2019/02/21 */
+class LifecycleInjectionHelper : ILifecycleOwnerInjectionHelper<Redux.State> {
+  override fun inject(injector: IPropInjector<Redux.State>, owner: LifecycleOwner) {
+    when (owner) {
+      is Fragment1 -> injector.injectLifecycle(Unit, owner, Fragment1)
+      is Fragment2 -> injector.injectLifecycle(Unit, owner, Fragment2)
+      is Fragment3 -> injector.injectLifecycle(Unit, owner, Fragment3)
+    }
+  }
+}
