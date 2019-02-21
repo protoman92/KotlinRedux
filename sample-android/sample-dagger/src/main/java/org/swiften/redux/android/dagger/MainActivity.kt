97bd2a09a6b4feb642c39ef86cc5863c805f7074
Hai Pham <swiften.svc@gmail.com>
2019-02-21 15:59:05 +0800

    chore(android-sample): initialize sample-dagger module

---
+++ sample-android/sample-dagger/src/main/java/org/swiften/redux/android/dagger/MainActivity.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) haipham 2019. All rights reserved.
+ * Any attempt to reproduce this source code in any form shall be met with legal actions.
+ */
+
+package org.swiften.redux.android.dagger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/** Created by haipham on 26/1/19 */
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    this.setContentView(R.layout.main_activity)
+    this.supportActionBar?.hide()
+
+    if (savedInstanceState == null) {
+      this.supportFragmentManager
+        .beginTransaction()
+        .replace(R.id.fragment, Fragment1())
+        .commit()
+    }
+  }
+}
