bba900ee9e493c9aaf48f56513789416791b1fe2
Hai Pham <swiften.svc@gmail.com>
2019-02-21 16:07:23 +0800

    feat(android-sample): set up redux injections for Fragment1/2/3

--- sample-android/sample-dagger/src/main/java/org/swiften/redux/android/dagger/Fragment3.kt
+++ sample-android/sample-dagger/src/main/java/org/swiften/redux/android/dagger/Fragment3.kt
@@ -6,6 +6,31 @@
 package org.swiften.redux.android.dagger
 
 import androidx.fragment.app.Fragment
+import org.swiften.redux.core.DefaultSubscriberIDProvider
+import org.swiften.redux.core.IActionDispatcher
+import org.swiften.redux.core.ISubscriberIDProvider
+import org.swiften.redux.ui.IPropContainer
+import org.swiften.redux.ui.IPropLifecycleOwner
+import org.swiften.redux.ui.IPropMapper
+import org.swiften.redux.ui.NoopPropLifecycleOwner
+import org.swiften.redux.ui.ObservableReduxProp
 
 /** Created by viethai.pham on 2019/02/21 */
-class Fragment3 : Fragment()
+class Fragment3 : Fragment(),
+  ISubscriberIDProvider by DefaultSubscriberIDProvider(),
+  IPropContainer<Fragment3.S, Fragment3.A>,
+  IPropLifecycleOwner<Fragment3.ILocalState, Unit> by NoopPropLifecycleOwner() {
+  companion object : IPropMapper<ILocalState, Unit, S, A> {
+    override fun mapState(state: ILocalState, outProp: Unit): S = S()
+    override fun mapAction(dispatch: IActionDispatcher, outProp: Unit): A = A()
+  }
+
+  interface ILocalState {
+    val flow3: S
+  }
+
+  class S
+  class A
+
+  override var reduxProp by ObservableReduxProp<S, A> { _, _ -> }
+}
--- sample-android/sample-dagger/src/main/java/org/swiften/redux/android/dagger/Redux.kt
+++ sample-android/sample-dagger/src/main/java/org/swiften/redux/android/dagger/Redux.kt
@@ -11,7 +11,15 @@ import java.io.Serializable
 
 /** Created by viethai.pham on 2019/02/21/2 */
 object Redux {
-  data class State(val dummy: Int = 0) : Serializable
+  data class State(
+    val dummy: Int = 0,
+    override val flow1: Fragment1.S = Fragment1.S(),
+    override val flow2: Fragment2.S = Fragment2.S(),
+    override val flow3: Fragment3.S = Fragment3.S()
+  ) : Serializable,
+    Fragment1.ILocalState,
+    Fragment2.ILocalState,
+    Fragment3.ILocalState
 
   object Reducer : IReducer<State> {
     override fun invoke(p1: State, p2: IReduxAction): State = p1
