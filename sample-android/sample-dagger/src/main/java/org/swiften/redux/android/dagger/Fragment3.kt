642fcf88d8d2508da1f4d86770017aeed0ea098e
Hai Pham <swiften.svc@gmail.com>
2019-02-21 16:16:05 +0800

    feat(android-sample): add basic layout for fragment1/2/3

--- sample-android/sample-dagger/src/main/java/org/swiften/redux/android/dagger/Fragment3.kt
+++ sample-android/sample-dagger/src/main/java/org/swiften/redux/android/dagger/Fragment3.kt
@@ -5,6 +5,10 @@
 
 package org.swiften.redux.android.dagger
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import org.swiften.redux.core.DefaultSubscriberIDProvider
 import org.swiften.redux.core.IActionDispatcher
@@ -33,4 +37,12 @@ class Fragment3 : Fragment(),
   class A
 
   override var reduxProp by ObservableReduxProp<S, A> { _, _ -> }
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    return inflater.inflate(R.layout.fragment_3, container, false)
+  }
 }
