7085b1a1192c265a0efe062927fe6aecf3f2e3bc
Hai Pham <swiften.svc@gmail.com>
2019-03-03 15:41:59 +0800

    feat(common-core): add BatchAction and BatchDispatchMiddleware to handle batches of actions

---
+++ common/common-core/src/main/kotlin/org/swiften/redux/core/BatchDispatchMiddleware.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) haipham 2019. All rights reserved.
+ * Any attempt to reproduce this source code in any form shall be met with legal actions.
+ */
+
+package org.swiften.redux.core
+
+/** Created by viethai.pham on 2019/03/03 */
+/**
+ * An [IReduxAction] that contains multiple other [IReduxAction] instances that can be dispatched
+ * individually.
+ */
+class BatchAction(internal val actions: Collection<IReduxAction>) : IReduxAction {
+  constructor(vararg actions: IReduxAction) : this(actions.toList())
+}
+
+/**
+ * [IMiddleware] that detects if an action if a [BatchAction]. If it is, access [BatchAction.actions]
+ * and dispatch them individually.
+ */
+class BatchDispatchMiddleware internal constructor() : IMiddleware<Any> {
+  companion object {
+    fun create(): IMiddleware<Any> = BatchDispatchMiddleware()
+  }
+
+  override fun invoke(p1: MiddlewareInput<Any>): DispatchMapper {
+    return { wrapper ->
+      DispatchWrapper.wrap(wrapper, "batch") { action ->
+        wrapper.dispatch(action)
+
+        when (action) {
+          is BatchAction -> action.actions.map { p1.dispatch(it) }
+        }
+
+        EmptyJob
+      }
+    }
+  }
+}
