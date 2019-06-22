67cf7492df65bf10004d689fdaa06b0afae530db
Hai Pham <swiften.svc@gmail.com>
2019-06-22 09:13:32 +0800

    feat(middleware): remove async middleware in favor of using scheduler for saga input

--- common/common-core/src/test/kotlin/org/swiften/redux/core/AsyncJobTest.kt
+++ common/common-core/src/test/kotlin/org/swiften/redux/core/AsyncJobTest.kt
@@ -11,49 +11,12 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotEquals
 import org.junit.Assert.assertThrows
 import org.junit.Test
-import java.util.Random
 
 /** Created by haipham on 2019/02/17 */
 class AsyncJobTest {
-  @Test
-  fun `Dispatching with async middleware should work correctly`() {
-    // Setup
-    class Action(val value: Int) : IReduxAction
-
-    val store = applyMiddlewares<Int>(
-      AsyncMiddleware.create(),
-      AsyncMiddleware.create(),
-      AsyncMiddleware.create()
-    )(FinalStore(-1) { s, a -> when (a) {
-      is Action -> a.value
-      else -> s
-    } })
-
-    val rand = Random()
-
-    // When
-    val iterations = 1000
-
-    val jobs = (0 until iterations).map {
-      GlobalScope.launch(Dispatchers.IO, start = CoroutineStart.LAZY) {
-        store.dispatch(Action(rand.nextInt(1000))).await()
-      }
-    }
-
-    runBlocking {
-      jobs.forEach { it.join() }
-
-      // Then
-      assertNotEquals(store.lastState(), -1)
-    }
-  }
-
   @Test
   fun `Batch job await with timeout should throw error on time out`() {
     // Setup
